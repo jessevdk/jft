dd6bc6c6363f8270c64530a4384041ec4baf975a
Jesse van den Kieboom <jesse@icecrew.nl>
2009-04-17 09:25:10 +0200

    Added easy insert-text range like signal

--- plugin/jft/BufferUtils.py
+++ plugin/jft/BufferUtils.py
@@ -31,5 +31,47 @@ class BufferUtils:
 		return (self.create_mark(None, start, True),
 				self.create_mark(None, end, False))
 
+	def connect_insert_text(self, handler):
+		handlers = self.buffer.get_data('BufferUtilsInsertTextHandler')
+		
+		if handlers == None:
+			id1 = self.buffer.connect('insert-text', self.on_insert_text)
+			id2 = self.buffer.connect_after('insert-text', self.on_insert_text_after)
+		
+			self.buffer.set_data('BufferUtilsInsertTextHandler', {'ids': [id1, id2], 'handlers': [handler]})
+		else:
+			handlers['handlers'].append(handler)
+	
+	def disconnect_insert_text(self, handler):
+		handlers = self.buffer.get_data('BufferUtilsInsertTextHandler')
+		
+		if handlers == None:
+			return
+
+		if handler in handlers['handlers']:
+			del handlers['handlers'][handler]
+			
+			if len(handlers['handlers']) == 0:
+				for i in handlers['ids']:
+					self.buffer.disconnect(i)
+				
+				self.buffer.set_data('BufferUtilsInsertTextHandler', None)
+
 	def __getattr__(self, name):
 		return getattr(self.buffer, name)
+
+	def on_insert_text(self, buf, location, text, length):
+		self._insert_text_start = buf.create_mark(None, location, True)
+	
+	def on_insert_text_after(self, buf, location, text, length)
+		handlers = self.buffer.get_data('BufferUtilsInsertTextHandler')
+
+		piter = self.get_iter_at_mark(self._insert_text_start)
+		
+		for handler in handlers['handlers']:
+			start = piter.copy()
+			end = location.copy()
+			
+			handler(start, end)
+		
+		self.delete_mark(self._insert_text_start)
