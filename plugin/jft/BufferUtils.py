0fe3bba2d3cd91a4b5eee9a400244403b101d010
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2011-01-03 18:25:16 +0100

    Remove trailing whitespace

--- plugin/jft/BufferUtils.py
+++ plugin/jft/BufferUtils.py
@@ -2,28 +2,28 @@ import re
 
 class BufferUtils:
 	def __init__(self, buf):
-		self.buffer = buf	
-	
+		self.buffer = buf
+
 	def line_at_iter(self, start):
 		start = start.copy()
 
 		start.set_line_offset(0)
 		end = start.copy()
-		
+
 		if not end.ends_line():
 			end.forward_to_line_end()
-	
+
 		return start.get_slice(end)
 
 	def line_at_offset(self, offset):
 		return self.line_at_iter(self.buffer.get_iter_at_line(offset))
-	
+
 	def insert_iter(self):
 		return self.buffer.get_iter_at_mark(self.buffer.get_insert())
 
 	def current_line(self):
 		return self.line_at_iter(self.insert_iter())
-	
+
 	def move_mark_to_mark(self, mark, target):
 		self.buffer.move_mark(mark, self.buffer.get_iter_at_mark(target))
 
@@ -33,58 +33,58 @@ class BufferUtils:
 
 	def delete_mark_range(self, r):
 		ret = []
-		
+
 		for i in r:
 			ret.append(self.get_iter_at_mark(i))
 			self.delete_mark(i)
-		
+
 		return ret
 
 	def iter_skip_space(self, piter):
 		while True:
 			c = piter.get_char()
-	
+
 			if not c.isspace() or c in ["\n", "\0"]:
 				return
-			
+
 			piter.forward_char()
-				
+
 	def _do_block_insert_text(self, handler, block):
 		handlers = self.buffer.get_data('BufferUtilsInsertTextHandler')
-		
+
 		if handlers and handler in handlers['handlers']:
 			handlers['handlers'][handler] = not block
 
 	def block_insert_text(self, handler):
 		self._do_block_insert_text(handler, True)
-		
+
 	def unblock_insert_text(self, handler):
 		self._do_block_insert_text(handler, False)
 
 	def connect_insert_text(self, handler):
 		handlers = self.buffer.get_data('BufferUtilsInsertTextHandler')
-		
+
 		if handlers == None:
 			id1 = self.buffer.connect('insert-text', self.on_insert_text)
 			id2 = self.buffer.connect_after('insert-text', self.on_insert_text_after)
-		
+
 			self.buffer.set_data('BufferUtilsInsertTextHandler', {'ids': [id1, id2], 'handlers': {handler: True}})
 		else:
 			handlers['handlers'][handler] = True
-	
+
 	def disconnect_insert_text(self, handler):
 		handlers = self.buffer.get_data('BufferUtilsInsertTextHandler')
-		
+
 		if handlers == None:
 			return
 
 		if handler in handlers['handlers']:
 			del handlers['handlers'][handler]
-			
+
 			if len(handlers['handlers']) == 0:
 				for i in handlers['ids']:
 					self.buffer.disconnect(i)
-				
+
 				self.buffer.set_data('BufferUtilsInsertTextHandler', None)
 
 	def __getattr__(self, name):
@@ -98,7 +98,7 @@ class BufferUtils:
 		handlers = self.buffer.get_data('BufferUtilsInsertTextHandler')
 
 		other = buf.create_mark(None, location, False)
-		
+
 		for handler in handlers['handlers']:
 			if handlers['handlers'][handler]:
 				start = self.get_iter_at_mark(self._insert_text_start)
